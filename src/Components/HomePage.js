acb6b542acef7da4c85afdd47fd6d2e4e79100e3
JacksonBey <jacksongrowson@gmail.com>
2020-11-06 10:21:08 -0800

    added a homepage component

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HomePage extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Hi from HomePage</h1>
+            </div>
+        )
+    }
+}
