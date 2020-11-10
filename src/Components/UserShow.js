4fc84ea0fc1a68acd0c2a69f06b6261245fb81ed
JacksonBey <jacksongrowson@gmail.com>
2020-11-10 11:58:58 -0800

    added edit function

--- src/Components/UserShow.js
+++ src/Components/UserShow.js
@@ -2,6 +2,75 @@ import React, {Component} from 'react';
 
 export default class UserShow extends Component {
 
+    handleClick = () => {
+        this.setState({
+            edit: true
+        })
+    }
+
+
+    state={
+        edit:false,
+        username: "",
+        password: "",
+        name: "",
+        bio: "",
+        image: ""
+    }
+
+    handleChange= (e) => {
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    handleSubmit=(e) => {
+        e.preventDefault()
+        this.props.handleSubmit(this.state)
+    }
+
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:3001/api/v1/users/${this.props.user.data.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            user: {
+                username: this.state.username,
+                password: this.state.password,
+                name: this.state.name,
+                bio: this.state.bio,
+                image: this.state.image
+            }
+          })
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          this.setState({
+              edit: false
+          })
+        })
+      }
+    
+    
+    
+      handleSignUp = (info) => {
+        console.log('sign up')
+        this.handleAuthFetch(info,'http://localhost:3001/api/v1/users')
+      }
+    
+    
+
+
+
+
+
+
+
 
 
 
@@ -12,7 +81,40 @@ export default class UserShow extends Component {
         return(
             <div>
                 <h1>Hi {username}</h1>
+                <img scr={image} />
+                <p>aka {name}</p>
+                <p> bio: {bio}</p>
                 <button onClick={this.props.handleLogout}>Logout</button>
+
+                {this.state.edit ?
+                
+                <form onSubmit={this.handleSubmit}>
+                {/* <label>Username
+                <input name="username" value={this.state.username} placeholder={username} onChange={this.handleChange}/>
+                </label>
+                <br/>
+                <label> New Password
+                <input name="password" value={this.state.password} onChange={this.handleChange}/>
+                </label>
+                <br/> */}
+                <label>name
+                <input name="name" value={this.state.name} placeholder={name} onChange={this.handleChange}/>
+                </label>
+                <br/>
+                <label>bio
+                <input name="bio" value={this.state.bio} placeholder={bio} onChange={this.handleChange}/>
+                </label>
+                <br/>
+                <label>Profile pic
+                <input name="image" value={this.state.image} placeholder={image} onChange={this.handleChange}/>
+                </label>
+                <br/>
+                <input type="submit" value="submit" />
+            </form>
+
+                
+                : 
+                <button onClick={this.handleClick}>Edit account</button>}
             </div>
         )} else {
             return <div>user not found</div>
